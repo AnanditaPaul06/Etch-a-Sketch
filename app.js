dfd92ef31fceac18a1e08105f38836e724188e02
Anandita Paul <paulanandita06@gmail.com>
2026-09-06 16:43:35 +0530

    adding js file

---
+++ app.js
@@ -0,0 +1,64 @@
+const screen = document.getElementById("screen");
+
+let sizeSlider = document.getElementById("sizeSlider");
+let size = document.getElementById("size");
+
+let colorPicker = document.getElementById("colorPicker");
+let color = "red";
+let randomColor=false;
+let isDrawing = false;
+// creating grid
+function createGrid() {
+    screen.innerHTML = "";
+    let gridSize = Number(sizeSlider.value);
+    let cellNo = gridSize * gridSize;
+    screen.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < cellNo; i++) {
+        let newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        screen.appendChild(newCell);
+        newCell.addEventListener("mousedown", function() {
+            isDrawing = true;
+            newCell.style.backgroundColor = color;
+        });
+
+        newCell.addEventListener("mouseover", function() {
+            if (isDrawing) {
+                if(!randomColor){
+                    newCell.style.backgroundColor = color;
+                }
+                else{
+                    newCell.style.backgroundColor=random();
+                }
+            }
+        });
+
+        newCell.addEventListener("mouseup", function() {
+            isDrawing = false;
+        });
+    }
+}
+sizeSlider.addEventListener("input", function() {
+    size.textContent = sizeSlider.value;
+    createGrid();
+
+});
+colorPicker.addEventListener("input", function() {
+    color = colorPicker.value;
+});
+const eraser=document.getElementById("eraser");
+eraser.addEventListener("click",function(){
+    color="white";
+})
+const clearScreen=document.getElementById("clear");
+clearScreen.addEventListener("click",function(){
+    createGrid();
+})
+const random=document.getElementById("random");
+random.addEventListener("click",function random(){
+    let rand1=Math.floor(Math.random()*256);
+    let rand2=Math.floor(Math.random()*256);
+    let rand3=Math.floor(Math.random()*256);
+    color=`rgb(${rand1},${rand2},${rand3})`;
+})
+createGrid();
